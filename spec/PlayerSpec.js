cc41db72db9d00239e656d8a49d5a49f6c55ea74
GiorgioNatili <g.natili@gnstudio.com>
2014-07-09 12:59:42 -0400

    initial  implementation

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,61 +1,62 @@
-define(["js/Player"], function(Player) {
+define(["../js/Player", "../js/Song"], function (player, song) {
 
-describe("Player", function() {
-  var player;
-  var song;
+    describe("Player", function () {
 
-  beforeEach(function() {
-    player = new Player();
-    song = new Song();
-  });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
+        beforeEach(function () {
 
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
+            player.reset();
+            song.reset();
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
+        });
+
+        it("should be able to play a Song", function () {
+
+            player.play(song);
+            expect(player.currentlyPlayingSong).toEqual(song);
+
+        });
+
+        describe("when song has been paused", function () {
+            beforeEach(function () {
+                player.play(song);
+                player.pause();
+            });
+
+            it("should indicate that the song is currently paused", function () {
+                expect(player.isPlaying).toBeFalsy();
+
+            });
+
+            it("should be possible to resume", function () {
+                player.resume();
+                expect(player.isPlaying).toBeTruthy();
+                expect(player.currentlyPlayingSong).toEqual(song);
+            });
+        });
+
+        // demonstrates use of spies to intercept and test method calls
+        it("tells the current song if the user has made it a favorite", function () {
+
+            spyOn(song, 'persistFavoriteStatus');
+
+            player.play(song);
+            player.makeFavorite();
+
+            expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+
+        });
+
+        //demonstrates use of expected exceptions
+        describe("#resume", function () {
+
+            it("should throw an exception if song is already playing", function () {
+                player.play(song);
+
+                expect(function () {
+                    player.resume();
+                }).toThrowError("song is already playing");
+            });
+        });
     });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
-  });
-});
 });
